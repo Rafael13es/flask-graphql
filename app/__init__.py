e9503ee147f274ea6dc5de7875fc6a8d52e05d8b
Rafael De Hermoso <rdehermoso@banistmo.com>
2023-07-15 00:23:01 -0500

    Creating app configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,20 @@
+"""App init"""
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from config import config
 
 db = SQLAlchemy()
+
+
+def create_app(config_name):
+    """
+    Create the app instance based on the environment
+        :parameter str config_name: configuration name
+        :return: a Flask object
+    """
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    db.init_app(app)
+    return app
